2ad863009a714148682311d425fab9e4f369eb2b
Alex Holovach <alex.holovach@kubiks.ai>
2025-07-11 21:37:06 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default async function Page() {
   console.error('Error from page')
   console.warn('Warn from page')
   console.debug('Debug from page')
+  console.log('Log with attributes', {
+    hello: 'world',
+  })
 
   return <TodoApp />
 }
